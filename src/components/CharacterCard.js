169149aa03d467785adaa3ee8bae10258cb2d0f0
74ultra <r.barnhill@protonmail.com>
2019-08-23 09:41:36 -0700

    Initial commit. Character card functionality (no styling)

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,34 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  
+  console.log(props.name)
+  
+  return (
+    
+    
+
+    <div>
+      <img src={props.charImg} alt={props.name} />
+      <p>Name: {props.name}</p>
+      <p>Species: {props.species}</p>
+      <p>Status: {props.status}</p>
+      <p>Location: {props.location}</p>
+      <p>Origin: {props.origin}</p>
+      
+
+    </div>
+
+    );
 }
+
+
+/*
+<img src={props.charImg} alt={props.name}/>
+      <p>Name: {props.name}</p>
+      <p>Species: {props.species}</p>
+      <p>Status: {props.status}</p>
+      <p>Location: {props.location}</p>
+
+
+*/
